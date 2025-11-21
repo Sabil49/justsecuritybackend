9d3bab1ef67c96e4f1a2246b9e157f1cfa9d39c9
Sabil49 <md.sabeel10@gmail.com>
2025-11-21 16:45:21 +0530

    change 4

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              Templates
+              Templates Templates Templates
             </a>{" "}
             or the{" "}
             <a
