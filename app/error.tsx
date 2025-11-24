6992ef497c5d556d646b46a094cf9e6982e05f4f
Sabil49 <md.sabeel10@gmail.com>
2025-11-24 18:17:20 +0530

    new changes 13

---
+++ app/error.tsx
@@ -0,0 +1,8 @@
+export default function Error() {
+    return (
+        <div style={{ padding: "2rem", textAlign: "center" }}>
+            <h1>Oops! An error occurred.</h1>
+            <p>We're sorry for the inconvenience. Please try again later.</p>
+        </div>
+    );
+}
---
+++ app/global-error.tsx
@@ -0,0 +1,8 @@
+export default function GlobalError() {
+    return (
+        <div style={{ padding: "2rem", textAlign: "center" }}>
+            <h1>Something went wrong!</h1>
+            <p>Please try refreshing the page or come back later.</p>
+        </div>
+    );
+}
